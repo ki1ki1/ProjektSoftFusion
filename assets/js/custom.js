f4d334a06b7ae4fb45913ac27aac5545996fd294
ki1ki1 <wika.041199@gmail.com>
2022-03-01 11:49:12 +0100

    filtry

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-    let status = 1;
     $(".mobile__hamburger").click(function(){
         $("nav .menu__center").toggle("fast");
         $("nav .menu__right").toggle("fast");
+    });
+    $(".header__button").click(function(){
+        $(".header__button").removeClass("active");
+        $(this).addClass("active");
     });
 });
     
